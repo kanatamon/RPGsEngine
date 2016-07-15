2256d44fd8bd0bff0d069c7222f9769b3c98fbbd
kanatamon <ponde.moonlord@gmail.com>
2016-07-15 19:20:45 +0700

    add property setup and calculation function

--- src/character.js
+++ src/character.js
@@ -3,12 +3,8 @@
 class Character {
 	constructor(name) {
 		this.name = name
-		this.status = {
-			'str': 1,
-			'agi': 1,
-			'int': 1
-		}
-	 	this.properties = {
+
+		this._properties = {
 			'level': 1,
 			'exp': 1,
 			'attk': 1,
@@ -18,6 +14,45 @@ class Character {
 			'hp': 1,
 			'mp': 1
 		}
+
+		this._status = {
+			'str': 1,
+			'agi': 1,
+			'int': 1
+		}
+	}
+
+	static calculateLevel(experience) {
+		if(experience < 1000) {
+			return 1
+		}
+		else if(experience > 99000) {
+			return 99
+		}
+
+		return Math.floor(experience / 1000)
+	}
+
+	static calculateAttk(playerProperties, itemProperties) {
+		//formula attk = (str * 2.2) + (agi * 0.8) + (item.weight * 0.4) + item.attk 
+		return (playerProperties.str * 2.2) + (playerProperties.agi * 0.8) + (itemProperties.weight * 0.4) + itemProperties.attk 
+	}
+
+	static calculateDef(playerProperties, itemProperties) {
+		//FORMULA: def = (agi * 1.1) + (int * 1.1) + item.def
+		return (playerProperties.agi * 1.1) + (playerProperties.int * 1.1) + itemProperties.def
+	}
+
+	get properties() {
+		return this._properties
+	}
+
+	get level() {
+		return this._properties.level
+	}
+
+	get status() {
+		return this._status
 	}
 }
 
--- test/character_test.js
+++ test/character_test.js
@@ -1,20 +1,20 @@
 var expect = require('chai').expect
 
-describe('CHARACTER CREATION', function() {
+describe('CHARACTER CREATION SETUP', function() {
 	var Character = require('../src/character')
-		var character
+	var character
 
 	beforeEach(function() {
 		character = new Character('lnw')
 	})
 
-	describe('characters name', function() {
+	describe('name', function() {
 		it('the characters name should be string', function() {
 			expect(character.name).to.be.a('string')
 		})
 	})
 
-	describe('characters status', function() {
+	describe('status', function() {
 		it('should include 3 status', function() {
 			expect(Object.keys(character.status).length).to.equal(3)
 		})
@@ -26,7 +26,7 @@ describe('CHARACTER CREATION', function() {
 		})
 	})
 
-	describe('characters properties', function() {
+	describe('properties', function() {
 		it('should include 8 properties', function() {
 			expect(Object.keys(character.properties).length).to.equal(8)
 		})
@@ -41,7 +41,116 @@ describe('CHARACTER CREATION', function() {
 			expect(character.properties).to.have.property('hp').and.be.a('number')
 			expect(character.properties).to.have.property('mp').and.be.a('number')
 		})
-
-		
 	})
 })
+
+
+
+describe('CHARACTER PROPERTY CALCULATION', function() {
+	var Character = require('../src/character')
+	var playerStatus
+	var itemProp
+
+	beforeEach(function() {
+		playerStatus = {
+			str: 1,
+			agi: 1,
+			int: 1,
+		}
+
+		itemProp = {
+			attk: 12,
+			mattk: 5,
+			weight: 10,
+			def: 2,
+			hp: 10,
+			mp: 12
+		}
+	})
+
+	describe('calculateLevel', function() {
+		it('should calculate correctly', function() {
+			//FORMULA: level = exp / 1000
+			expect(Character.calculateLevel(1000)).to.equal(1)
+			expect(Character.calculateLevel(2499.99)).to.equal(2)
+			expect(Character.calculateLevel(9000)).to.equal(9)
+		})
+
+		it('should return level 1 when exp is less than 1000', function() {
+			expect(Character.calculateLevel(900)).to.equal(1)
+			expect(Character.calculateLevel(-9)).to.equal(1)
+		})
+
+		it('should return level 99 when exp is more than 99,000', function() {
+			expect(Character.calculateLevel(100000)).to.equal(99)
+			expect(Character.calculateLevel(99000)).to.equal(99)
+			expect(Character.calculateLevel(99000.01)).to.equal(99)
+			expect(Character.calculateLevel(99001)).to.equal(99)
+		})
+	})
+
+	describe('calculateAttk', function() {
+		it('should calculate correctly', function() {
+			//FORMULA: attk = (str * 2.2) + (agi * 0.8) + (item.weight * 0.4) + item.attk 
+			//attk = 2.2 + 0.8 + 4 + 12 -> = **19
+			expect(Character.calculateAttk(playerStatus, itemProp)).to.equal(19)
+		})
+	})
+
+	describe('calculateDef', function() {
+		it('should calculate correctly', function() {
+			//FORMULA: def = (agi * 1.1) + (int * 1.1) + item.def
+			//def = 1.1 + 1.1 + 2
+			expect(Character.calculateDef(playerStatus, itemProp)).to.equal(4.2)
+		})
+	})
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
